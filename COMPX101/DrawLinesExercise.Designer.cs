7a2f39cbfb39d9d21aae6adba54a36729feedb35
Theo at home <theobraith@gmail.com>
2021-03-17 00:17:32 +1300

    Add project files.

---
+++ COMPX101/DrawLinesExercise.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace COMPX101
+{
+    partial class FormDrawLines
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDrawLine = new System.Windows.Forms.Button();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDrawLine
+            // 
+            this.buttonDrawLine.Location = new System.Drawing.Point(12, 402);
+            this.buttonDrawLine.Name = "buttonDrawLine";
+            this.buttonDrawLine.Size = new System.Drawing.Size(776, 23);
+            this.buttonDrawLine.TabIndex = 0;
+            this.buttonDrawLine.Text = "Draw Line";
+            this.buttonDrawLine.UseVisualStyleBackColor = true;
+            this.buttonDrawLine.Click += new System.EventHandler(this.buttonDrawLine_Click);
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(776, 384);
+            this.pictureBoxDisplay.TabIndex = 1;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Scratch Pad";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FormDrawLines
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.buttonDrawLine);
+            this.Name = "FormDrawLines";
+            this.Text = "Scratch Pad";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDrawLine;
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ COMPX101/DrawLinesExercise.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMPX101
+{
+    public partial class FormDrawLines : Form
+    {
+        public FormDrawLines()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDrawLine_Click(object sender, EventArgs e)
+        {
+            //Version 1 Scratch Pad.
+
+            //Create a graphics object called paper which is
+            //Connected to the picturebox specified.
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            //Create a pen object called pen1
+            //In the colour blue with a width of 5
+            Pen pen1 = new Pen(Color.Blue, 5);
+            //Create a red pen with a width of 2
+            //Pen pen2 = new Pen(Color.Red, 2);
+
+
+            //Draws a line using the pen specified and
+            //Using the coordinates specified.
+            paper.DrawLine(pen1, 10, 30, 100, 200);
+
+            //Change the colour of pen1 to red
+            pen1.Color = Color.Red;
+
+            //Change the width of the pen to 2
+            pen1.Width = 2;
+
+            //Draw a red line
+            paper.DrawLine(pen1, 150, 100, 250, 200);
+
+            paper.DrawLine(pen1, 0, 0, pictureBoxDisplay.Width, pictureBoxDisplay.Height);
+
+            ////Ex 6 - It draws a triangle
+            //paper.DrawLine(pen1, 100, 50, 150, 100);
+            //paper.DrawLine(pen1, 150, 100, 50, 100);
+            //paper.DrawLine(pen1, 50, 100, 100, 50);
+
+            //Ex 8 - need to change all the coordinates
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
